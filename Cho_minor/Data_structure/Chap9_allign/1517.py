6dd030e87921dabc7d8bbdf6dd66aca08e7f1f04
picacyano <picacyano@gmail.com>
2024-08-18 15:51:37 +0900

    iwannagohom

--- Cho_minor/Data_structure/Chap9_allign/1517.py
+++ Cho_minor/Data_structure/Chap9_allign/1517.py
@@ -1,25 +1,27 @@
-len : int = int(input());
-arr : list = list(input().split(sep = ' '));
+import sys
 
-rdy : bool;
-i : int;
-j : int;
-tmp : int;
-swap : int = 0;
-for i in range(0, len):
-    arr[i] = int(arr[i]);
+len = int(sys.stdin.readline())
+arr = list(sys.stdin.readline().split(sep = ' '))
+
+rdy : bool
+i : int
+j : int
+tmp : int
+swap : int = 0
 
 for i in range(0, len):
-    rdy = 1;
+    arr[i] = int(arr[i])
+
+for i in range(0, len):
+    rdy = 1
     for j in range(0, len - i - 1):
         tmp = arr[j]
         if(arr[j] > arr[j + 1]):
-            arr[j] = arr[j + 1];
-            arr[j + 1] = tmp;
-            rdy = 0;
-            swap =+ 1;
+            arr[j] = arr[j + 1]
+            arr[j + 1] = tmp
+            rdy = 0
 
     if(rdy == 1):
-        break;
+        break
 
-print(swap);
+print(j)
