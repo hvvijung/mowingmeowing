730f7ca993131c39beb7e1ab5a319ddc5be0b0c5
picacyano <picacyano@gmail.com>
2024-08-18 13:45:33 +0900

    assult commit

--- Cho_minor/Data_structure/Chap9_allign/23881.py
+++ Cho_minor/Data_structure/Chap9_allign/23881.py
@@ -9,12 +9,12 @@ temp : int
 
 for j in range(0, k - 1):
     largest =  int(arr[0])
-    for i in range(0, len - 1):
+    for i in range(0, len - 1 - j):
         if(int(arr[i]) >= largest):
             largest = int(arr[i]);
             largest_pos = i;
-    arr[largest_pos] = arr[len - j - 1];
-    arr[j - 1] = largest;
+    arr[largest_pos] = arr[len - 1 - j];
+    arr[len - j - 1] = largest;
 
 for i in range(0, len):
     print(arr[i])
